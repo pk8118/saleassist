620cba2a7ab3e879d0a42a02cc41bf4a97cb8b8d
purkharam <pkchoudhary09@yahoo.com>
2024-02-17 09:49:31 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import Products from './components/Products';
 import './App.css';
 
 const App = () => (
   <div className='container'>
     <Header/>
+    <Products/>
   </div>
   
 )
---
+++ src/components/Products/index.js
@@ -0,0 +1,14 @@
+import AllProductsSection from '../AllProductsSection'
+
+
+import './index.css'
+
+const Products = () => (
+  <>
+    <div className="product-sections">
+      <AllProductsSection />
+    </div>
+  </>
+)
+
+export default Products
